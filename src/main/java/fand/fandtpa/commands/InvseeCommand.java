c79b76d9c353227426db0fe86e8e67e1529ff7a8
FandMC <winme2002@outlook.com>
2024-08-31 11:31:59 +0800

    two

--- src/main/java/fand/fandtpa/commands/InvseeCommand.java
+++ src/main/java/fand/fandtpa/commands/InvseeCommand.java
@@ -2,6 +2,7 @@ package fand.fandtpa.commands;
 
 import fand.fandtpa.util.ChatColor;
 import fand.fandtpa.util.ConfigManager;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -21,12 +22,10 @@ public class InvseeCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if (!(sender instanceof Player)) {
+        if (!(sender instanceof Player player)) {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', configManager.getMessage("invsee_only_player")));
             return true;
         }
-
-        Player player = (Player) sender;
 
         if (args.length != 1) {
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', configManager.getMessage("invsee_usage")));
@@ -35,26 +34,41 @@ public class InvseeCommand implements CommandExecutor {
 
         Player target = Bukkit.getPlayer(args[0]);
 
-        if (target != null && target.isOnline()) {
-            openCustomInventoryView(player, target);
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', configManager.getMessage("invsee_viewing").replace("{target}", target.getName())));
-        } else {
+        if (target == null || !target.isOnline()) {
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', configManager.getMessage("invsee_player_not_found")));
+            return true;
         }
+
+        if (target.equals(player)) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', configManager.getMessage("invsee_cannot_view_own_inventory")));
+            return true;
+        }
+
+        openCustomInventoryView(player, target);
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', configManager.getMessage("invsee_viewing").replace("{target}", target.getName())));
 
         return true;
     }
 
     private void openCustomInventoryView(Player viewer, Player target) {
-        Inventory customInventory = Bukkit.createInventory(null, 54, target.getName() + configManager.getMessage("invsee_inventory_suffix"));
+        // 使用 Component 替代 String 创建标题
+        Inventory customInventory = Bukkit.createInventory(null, 54, Component.text(target.getName() + configManager.getMessage("invsee_inventory_suffix")));
 
         ItemStack[] inventoryContents = target.getInventory().getContents();
-        System.arraycopy(inventoryContents, 0, customInventory.getContents(), 0, Math.min(inventoryContents.length, 36));
+        for (int i = 0; i < 36; i++) {
+            if (inventoryContents[i] != null) {
+                customInventory.setItem(i, inventoryContents[i].clone());
+            }
+        }
 
-        customInventory.setItem(36, target.getInventory().getItemInOffHand());
+        customInventory.setItem(36, target.getInventory().getItemInOffHand().clone());
 
         ItemStack[] armorContents = target.getInventory().getArmorContents();
-        System.arraycopy(armorContents, 0, customInventory.getContents(), 45, armorContents.length);
+        for (int i = 0; i < armorContents.length; i++) {
+            if (armorContents[i] != null) {
+                customInventory.setItem(45 + i, armorContents[i].clone());
+            }
+        }
 
         viewer.openInventory(customInventory);
     }
