edd639acdaf1635a73295f4785b29750006118be
FandMC <winme2002@outlook.com>
2024-10-31 21:56:45 +0800

    优化体验

--- src/main/java/com/fand/commands/command/TprandomCommand.java
+++ src/main/java/com/fand/commands/command/TprandomCommand.java
@@ -2,13 +2,13 @@ package com.fand.commands.command;
 
 import com.fand.Main;
 import com.fand.util.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
@@ -26,6 +26,10 @@ public class TprandomCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (sender instanceof Player player) {
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&e正在寻找安全的位置，请稍候..."));
+
+            // 添加短暂的失明效果
+            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 60, 1, false, false));
+
             new BukkitRunnable() {
                 @Override
                 public void run() {
@@ -34,16 +38,28 @@ public class TprandomCommand implements CommandExecutor {
                         new BukkitRunnable() {
                             @Override
                             public void run() {
+                                // 传送玩家到安全位置
                                 player.teleport(randomLocation);
+
+                                // 发送成功消息并显示坐标
                                 player.sendMessage(ChatColor.translateAlternateColorCodes('&',
                                         "&a你已经被随机传送到安全位置！位置: &bX: " + randomLocation.getBlockX()
                                                 + " Y: " + randomLocation.getBlockY()
                                                 + " Z: " + randomLocation.getBlockZ()));
+
+                                // 在玩家传送后触发白烟效果
+                                spawnSmokeEffect(player.getLocation());
                             }
                         }.runTask(plugin);
                     } else {
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                                "&c无法找到安全位置进行随机传送，请稍后重试！"));
+                        // 如果找不到安全位置，提醒玩家失败
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                        "&c无法找到安全位置进行随机传送，请稍后重试！"));
+                            }
+                        }.runTask(plugin);
                     }
                 }
             }.runTaskAsynchronously(plugin);
@@ -61,7 +77,7 @@ public class TprandomCommand implements CommandExecutor {
         for (int attempts = 0; attempts < maxAttempts; attempts++) {
             double x = player.getLocation().getX() + random.nextInt(20001) - 10000;
             double z = player.getLocation().getZ() + random.nextInt(20001) - 10000;
-            double y = world.getHighestBlockYAt((int) x, (int) z) + 1; // 确保玩家不会在地下生成
+            double y = world.getHighestBlockYAt((int) x, (int) z) + 1;
             Location randomLocation = new Location(world, x, y, z);
 
             if (isSafeLocation(randomLocation)) {
@@ -85,6 +101,19 @@ public class TprandomCommand implements CommandExecutor {
     }
 
     private boolean isSolidGround(Material material) {
-        return material.isSolid() && !material.equals(Material.LAVA) && !material.equals(Material.CACTUS);
+        return material.isSolid() && !material.equals(Material.LAVA) && !material.equals(Material.CACTUS)
+                && !material.equals(Material.FIRE) && !material.equals(Material.MAGMA_BLOCK);
+    }
+
+    private void spawnSmokeEffect(Location location) {
+        World world = location.getWorld();
+        if (world != null) {
+            for (int i = 0; i < 20; i++) {
+                double offsetX = (Math.random() - 0.5) * 2.0;
+                double offsetY = Math.random();
+                double offsetZ = (Math.random() - 0.5) * 2.0;
+                world.spawnParticle(Particle.SMOKE, location.clone().add(offsetX, offsetY, offsetZ), 0);
+            }
+        }
     }
 }
